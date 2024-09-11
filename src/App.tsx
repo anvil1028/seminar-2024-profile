9c3d52cce88104ee65711cdf11c770ec7424201d
anvil1028 <anvil1028@snu.ac.kr>
2024-09-11 18:55:55 +0900

    Inital Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
+import './reset.css';
 import './App.css';
 
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import anvil from './anvil.jpg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <header></header>
+        <div className="info">
+          <img className="img" src={anvil} />
+          <div className="detail">
+            <p>이름 : 임형석</p>
+            <p>학과 : 컴퓨터공학부 24학번</p>
+            <p>나이 : 19</p>
+          </div>
+        </div>
+        <footer>
+          <p>연 나이는 20살입니다. MBTI는 INTP입니다. 잘 부탁드립니다.</p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
